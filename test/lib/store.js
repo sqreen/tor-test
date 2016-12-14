e7ba2a567ccfeacbc176ac4a91b8ea84d65deb9e
Vouill <vouillon.thibaud@gmail.com>
2016-12-14 20:45:49 +0100

    Auto refresh by number of days & test added

--- test/lib/store.js
+++ test/lib/store.js
@@ -169,5 +169,59 @@ describe('store', () => {
                 done();
             });
         });
+
+        it('should fetch the store from the API when a given interval of day from last API call is passed', { plan: 9 }, (done) => {
+
+            const Wreck = require('wreck');
+            const now = new Date();
+            const dateInPast = now.setDate(now.getDate() - 10);
+            Wreck.get = function (url, options, cb) {
+
+                expect(url).to.equal('http://sqreen-tor.s3-website-eu-west-1.amazonaws.com/sqreen/v0/ips/tor/exit_nodes');
+                expect(options).to.equal({ headers: { accept: 'application/json' }, json: true });
+                return cb(null, { statusCode: 200 },{
+                    date: dateInPast,
+                    list: [
+                        {
+                            v: 4,
+                            addr: '127.0.0.1'
+                        },
+                        {
+                            v: 6,
+                            addr: '::1'
+                        }
+                    ]
+                });
+            };
+
+            const Store = require('../../lib/store');
+            Store.refreshStoreOlderThan(8 ,(err) => {
+
+                expect(err).to.not.exist();
+                Store.isInStore('127.0.0.1', (err, res) => {
+
+                    expect(err).to.not.exist();
+                    expect(res).to.be.true();
+                    Store.isInStore('127.0.0.2', true, (err, res2) => {
+
+                        expect(err).to.not.exist();
+                        expect(res2).to.be.false();
+                        done();
+                    });
+                });
+            });
+
+        });
+
+        it('should return an error if wrong type is passed to refreshStoreOlderThan', { plan: 1 }, (done) => {
+
+            const Store = require('../../lib/store');
+            Store.refreshStoreOlderThan('not a number',(err) => {
+
+                expect(err).to.exist();
+                done();
+            });
+
+        });
     });
 });
